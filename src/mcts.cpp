a7aef86d1eab94b2ac8b53001868616547c84bbb
Atomu Naka <rotom86wash@gmail.com>
2025-06-07 00:09:48 +0900

    added: simulator files

--- src/mcts.cpp
+++ src/mcts.cpp
@@ -1,30 +1,75 @@
 #include <iostream>
+#include <limits>
 #include "mcts.h"
 #include "digitalcurling3/digitalcurling3.hpp"
+#include "simulator.h"
 
-MCTS_Node::MCTS_Node(MCTS_Node* parent, std::vector<ShotInfo> shot_candidates) {
+namespace dc = digitalcurling3;
 
+MCTS_Node::MCTS_Node(MCTS_Node* parent, std::vector<ShotInfo> shot_candidates, dc::GameState& const game_state)
+: parent(parent), untried_shots(shot_candidates), state(game_state), visits(0), wins(0), score(0.0) {
+    selected_shot = shot_candidates.front();
+	terminal = state.IsGameOver();
+	
 }
 
 bool MCTS_Node::is_fully_expanded() const{
-	return children.empty();
+	return untried_shots.empty();
 }
 MCTS_Node* MCTS_Node::select_best_child(double c = 1.41) {
+    MCTS_Node* best = nullptr;
+    double best_score = -std::numeric_limits<double>::infinity();
 
+    for (const auto& child : children) {
+        if (child->visits == 0) continue;
+        if (score > best_score) {
+            best_score = score;
+            best = child.get();
+        }
+    }
+    return best;
 }
 void MCTS_Node::expand() {
-
+    if (terminal) {
+        rollout();
+        return;
+    }
+    if (is_fully_expanded()) {
+        std::cerr << "Warning: Cannot expanded this node any more!" << "\n";
+        return;
+    }
+    // Pick one untried shot and create a new child node
+    ShotInfo shot = untried_shots.back();
+    untried_shots.pop_back();
+    dc::GameState next_state = getNextState(shot); // get next state through shotinfo
+    std::vector<ShotInfo> next_shots = generate_possible_shots_after(shot, next_state);
+    auto new_node = std::make_unique<MCTS_Node>(this, shot, next_state);
 }
-double MCTS_Node::rollout() {
-
+void MCTS_Node::rollout() {
+    if (!terminal) {
+        simulator_wrapper.run_simulation(state, selected_shot);
+        
+    }
 }
-double MCTS_Node::calculate_winrate() {
-
+double MCTS_Node::calculate_winrate() const {
+    if (visits == 0) {
+        return 0.0;
+    }
+    else {
+        return wins / visits;
+    }
 }
 void MCTS_Node::backpropagate(double w, int n) {
-
+    score += w;
+    visits += n;
+    if (parent != NULL) {
+        parent->visits++;
+        parent->backpropagate(w, n);
+    }
 }
-bool MCTS_Node::is_terminal() const {
+
+std::vector<ShotInfo> MCTS_Node::generate_possible_shots_after(const ShotInfo& shotinfo, dc::GameState& const game_state) {}
+dc::GameState MCTS_Node::getNextState(ShotInfo shotinfo) {
 
 }
 
--- src/mcts.h
+++ src/mcts.h
@@ -4,6 +4,7 @@
 
 #include "digitalcurling3/digitalcurling3.hpp"
 #include "structure.h"
+#include "simulator.h"
 #include <vector>
 namespace dc = digitalcurling3;
 
@@ -13,17 +14,23 @@ public:
 	MCTS_Node* parent;
 	std::vector<std::unique_ptr<MCTS_Node>> children;
 	int visits;
+	int wins;
 	double score;
-	dc::GameState node_state;
-	std::vector<ShotInfo> shot_candidates;
-	MCTS_Node(MCTS_Node* parent, std::vector<ShotInfo> shot_candidates);
+	dc::GameState state;
+	ShotInfo selected_shot;
+	std::vector<ShotInfo> untried_shots;
+	bool terminal;
+	SimulatorWrapper simulator_wrapper;
+	MCTS_Node(MCTS_Node* parent, std::vector<ShotInfo> shot_candidates, dc::GameState& const game_state);
 	bool is_fully_expanded() const;
 	MCTS_Node* select_best_child(double c = 1.41);
 	void expand();
-	double rollout();
-	double calculate_winrate();
+	void rollout();
+	double calculate_winrate() const ;
 	void backpropagate(double w, int n);
-	bool is_terminal() const;
+private:
+	std::vector<ShotInfo> MCTS_Node::generate_possible_shots_after(const ShotInfo& shotinfo, dc::GameState& const game_state);
+	dc::GameState MCTS_Node::getNextState(ShotInfo shotinfo);
 };
 
 class MCTS {
