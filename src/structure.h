5e7dee06cbcfcc84e264385e5f34f4cff193c856
Atomu Naka <rotom86wash@gmail.com>
2025-06-14 23:09:53 +0900

    removed: namespace

--- src/structure.h
+++ src/structure.h
@@ -2,15 +2,20 @@
 #ifndef _STRUCTURE_H
 #define _STRUCTURE_H
 
+#include <tuple>
+#include <vector>
+
 struct Position {
     float x;
     float y;
 };
+
 struct ShotInfo {
     float vx;
     float vy;
     int rot;
 };
+
 using LinkageRow = std::tuple<int, int, float, int>;
 using LinkageMatrix = std::vector<LinkageRow>;
 
